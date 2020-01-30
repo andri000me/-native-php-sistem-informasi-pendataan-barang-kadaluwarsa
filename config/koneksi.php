cac83e74215c45573c935003d5f6c2ec61aa15ac
baybay31 <60464109+baybay31@users.noreply.github.com>
2020-01-30 15:39:52 +0700

    Add files via upload

---
+++ config/koneksi.php
@@ -0,0 +1,5 @@
+<?php
+// mysqli_connect("hostname","user database","password","nama")
+$koneksi= mysqli_connect("localhost","root","","dbkadaluwarsa_source1") or die("Tidak bisa terhubungan ke database");
+
+
